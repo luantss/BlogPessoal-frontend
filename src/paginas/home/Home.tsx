e7e7db8528aacf6ac7e425e55285f09588435563
unknown <luan.teixeira.saramago@gmail.com>
2022-06-25 13:53:26 -0300

    criação do projeto react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        // caminho abreviado ou fragment 
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://digilandia.io/wp-content/uploads/2020/06/Dicas-para-ter-foco-no-home-office.jpg" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;

87d561abc0a15787d802601c62e1354204a4e532
unknown <luan.teixeira.saramago@gmail.com>
2022-07-15 10:48:42 -0300

    fix para o deploy

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // Este método é que irá montar e renderizar a tela por completo 
 // O elemento 'root' está dentro da div do index.html 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 reportWebVitals();
